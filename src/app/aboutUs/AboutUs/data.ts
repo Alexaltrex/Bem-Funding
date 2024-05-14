e01a46164def9b37c4693ac6dc739404b8a61371
AlexAltrex <alexaltrex@mail.ru>
2024-05-15 00:22:06 +0300

    fix

--- src/app/aboutUs/AboutUs/data.ts
+++ src/app/aboutUs/AboutUs/data.ts
@@ -1,26 +1,30 @@
 export const items = [
     {
         src: "/png/about us/0.png",
-        label: "Who are we",
+        label: "What We Offer",
         texts: [
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-        ]
+            "At BEM Funding, our mission is clear: to identify and empower the world's most talented traders. We understand the journey of a trader is filled with challenges and opportunities alike. Our goal is to fund those who not only have the skills to thrive in the competitive world of trading but also the dedication and resilience to continually excel. Our offering is designed for traders who envision themselves at the forefront of the industry, working with some of the most prestigious financial institutions worldwide."
+            ]
     },
     {
         src: "/png/about us/1.png",
-        label: "Why us",
+        label: "A Pathway to Recognition and Reward",
         texts: [
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-        ]
+            "Our commitment to you extends beyond mere funding. We recognize the hard work and dedication it takes to succeed in this field. As such, traders who demonstrate exceptional skill and the ability to secure payouts from our firm will not only receive immediate financial rewards but will also unlock opportunities for future benefits. Our reward system is built to acknowledge your achievements, both now and in the future, ensuring that your success is both celebrated and sustained."
+           ]
+    },
+    {
+        src: "/png/about us/0.png",
+        label: "Beyond Funding – A Glimpse into the Future",
+        texts: [
+            "While we take pride in the support and resources we currently offer, we are also excited about the future. Our vision for BEM Funding 2.0 is not just to fund traders but to forge a pathway to real-life opportunities within the trading industry and beyond. Imagine a future where your success with us opens doors to career advancements with financial institutions, collaborations, and roles that only a select few can dream of. While we're not ready to unveil all the details just yet, rest assured, we are working on innovative ways to elevate your trading career to heights previously unimagined."
+            ]
     },
     {
         src: "/png/about us/2.png",
-        label: "What we offer",
+        label: "Beyond Funding – A Glimpse into the Future",
         texts: [
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-        ]
+            "As we grow and evolve, our promise to you remains unwavering: to provide a platform that not only funds your trading endeavors but also supports your growth, rewards your achievements, and opens up a world of possibilities. Stay tuned for exciting developments on the horizon."
+            ]
     },
 ]
